870b8bfa6565c858e27fbbf3592c4a6554511167
Henry Daniel Hernandez Morales <a21110385@ceti.mx>
2024-09-19 22:35:35 -0600

    JAVA

---
+++ cenaFilosofos/main.java
@@ -0,0 +1,11 @@
+package cenaFilosofos;
+
+
+public class main {
+
+    public static void main(String[] args) {
+        ventanaFilosofos ventana = new ventanaFilosofos();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+}
